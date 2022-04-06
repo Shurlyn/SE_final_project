c9e7b4141224dd0dd87cfc29975d77b823a18734
Shurlyn <61921703+Shurlyn@users.noreply.github.com>
2022-04-06 13:05:47 -0400

    first commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
 })
+
+// module.exports = {
+//     pwa: {
+//     name: "SE final project",
+//     themColor:"#bd34eb"
+//   }
+// }
+
+// this should change colors, but how? Order is probably wrong.
